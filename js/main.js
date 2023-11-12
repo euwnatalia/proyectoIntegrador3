53af6f339397f9f4e5c64ecf3bd329d088fcfc1a
euwnatalia <euwnatalia@gmail.com>
2023-11-12 17:01:10 -0300

    creacion de visitanos.html
    primer intento de usar la api de ubicacion de maps en visitanos.html donde la idea es mostrar la ubicacion de san rafael y valle de uco y que hace un enlace tambien con una breve descripcion que te redirige a las paginas ya creadas

--- js/main.js
+++ js/main.js
@@ -8,3 +8,15 @@ document.addEventListener("DOMContentLoaded", () => {
         emailInput.value = '';
     });
 });
+
+function iniciarMap(){
+    var coord = {lat:-31.3549249 ,lng: -64.3731489};
+    var map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 10,
+      center: coord
+    });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+}
