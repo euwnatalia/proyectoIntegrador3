8eb4222ada2d14bb22747632d7b7a8ae739231e6
euwnatalia <euwnatalia@gmail.com>
2023-11-14 17:06:21 -0300

    detalles de js

--- js/visitanos.js
+++ js/visitanos.js
@@ -1,4 +1,4 @@
-//mapas de visitanos.html
+//mapas de visitanos.html san rafael
 function iniciarMapSR(){
     var coord = {lat:-34.5828004 ,lng: -68.42681};
     var map = new google.maps.Map(document.getElementById('mapSR'),{
