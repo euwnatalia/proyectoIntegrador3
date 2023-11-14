9d5d917c43fac7920213ad86927bc9fd917d236c
euwnatalia <euwnatalia@gmail.com>
2023-11-13 23:56:42 -0300

    error en main.js
    al haber un solo archivo js compartido en varias paginas todas las funciones se ejecutaba en cada carga de página y eso generaba error ya que no todas contienen lo mismo. se opto por hacer un archivo .js para cada necesidad de la pagina y asi evitar conflicto con los archivos.

--- js/contacto.js
+++ js/contacto.js
@@ -1,26 +1,3 @@
-//suscribirse a newsletter de la pagina en la pagina valleDeUco.html
-document.addEventListener("DOMContentLoaded", () => {
-    var newsletterSubmit = document.getElementById("newsletterSubmit");
-    newsletterSubmit.addEventListener("click", () => {
-        var emailInput = document.getElementById("newsletterEmail");
-        alert("email enviado correctamente: " + emailInput.value);
-
-        console.log("Email para suscribirse: " + emailInput.value);
-        emailInput.value = '';
-    });
-});
-//mapas de visitanos.html
-function iniciarMap(){
-    var coord = {lat:-31.3549249 ,lng: -64.3731489};
-    var map = new google.maps.Map(document.getElementById('map'),{
-      zoom: 10,
-      center: coord
-    });
-    var marker = new google.maps.Marker({
-      position: coord,
-      map: map
-    });
-}
 //formulario de contacto en la pagina contacto.html
 document.addEventListener("DOMContentLoaded", function() {
     var btnSiguiente = document.getElementById("btnSiguiente");
@@ -64,9 +41,19 @@ document.addEventListener("DOMContentLoaded", function() {
       document.getElementById("resumenEmail").textContent = "Email: " + email;
       document.getElementById("resumenExperiencia").textContent = "Experiencia: " + experiencia;
       document.getElementById("resumenDetalles").textContent = "Detalles: " + detalles;
+      
+      var resumen = `Resumen de la Consulta:
+      Nombre: ${nombre}
+      Apellido: ${apellido}
+      Provincia: ${provincia}
+      Teléfono: ${telefono}
+      Email: ${email}
+      Experiencia a Consultar: ${experiencia}
+      Detalles de la Consulta: ${detalles}`;
+      console.log(resumen);
     }
     btnEnviar.addEventListener("click", function() {
-      alert("Formulario enviado");
+      alert("Formulario enviado correctamente");
       resetearFormulario();
   });
   
